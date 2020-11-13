b8e96bcedc286e58a4896889536c03d077b11b8a
AntMan <xanthonyalvarezx@gmail.com>
2020-11-13 09:59:56 -0500

    fixing for deploy

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import React, { Component } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import './App.css';
-import { Button, Icon, } from "semantic-ui-react";
-import FormPage from './pages/FormPage'
-import Nav from './components/Nav'
-import Main from './pages/Main'
-import VideoPage from './pages/VideoPage'
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import { Button, Icon } from "semantic-ui-react";
+import FormPage from "./pages/FormPage";
+import Nav from "./components/Nav";
+import Main from "./pages/Main";
+import VideoPage from "./pages/VideoPage";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isClicked: false,
-    }
+    };
   }
   render() {
     return (
       <div className="App">
         <Router>
-
           <Nav />
           <Switch>
-            <Route exact path='/' component={Main} />
+            <Route exact path="/" component={Main} />
 
-            <Route path='/Videos' component={VideoPage} />
+            <Route path="/Videos" component={VideoPage} />
 
-            <Route path='/Subscribe' component={FormPage} />
+            <Route path="/Subscribe" component={FormPage} />
           </Switch>
-
         </Router>
         <footer>
-          <div className='socialButtonDiv'>
+          <div className="socialButtonDiv">
             <h4>Find us on Facebook and Instagram!</h4>
-            <Button color='facebook'>
-              <Icon name='facebook' /> <a href='https://www.facebook.com/SePaMountainBiking' target='_blank'>SEPA Mountsin Biking</a>
+            <Button color="facebook">
+              <Icon name="facebook" />{" "}
+              <a href="https://www.facebook.com/SePaMountainBiking" target="_blank" rel="noopener noreferrer">
+                SEPA Mountsin Biking
+              </a>
             </Button>
 
-            <Button color='instagram'>
-              <Icon name='instagram' /> <a href='https://www.instagram.com/sepa_mountain_biking/' target='_blank'>SEPA Mountsin Biking</a>
+            <Button color="instagram">
+              <Icon name="instagram" />{" "}
+              <a href="https://www.instagram.com/sepa_mountain_biking/" target="_blank" rel="noopener noreferrer">
+                SEPA Mountsin Biking
+              </a>
             </Button>
-          </div>¸
-                </footer>
+          </div>
+          ¸
+        </footer>
       </div>
-
     );
   }
 }
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react'
-import './videos.css'
+import React, { Component } from "react";
+import "./videos.css";
 
 class Videos extends Component {
+  render() {
+    return (
+      <div className="videos">
+        <ul id="videoList">
+          <li>
+            <span role="img">🔎</span>
+            <iframe
+              width="560"
+              height="315"
+              title="video1"
+              src="https://www.youtube.com/embed/4Q5b2r_sRX0"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </li>
 
+          <li>
+            <span role="img">🔎</span>
+            <iframe
+              width="560"
+              height="315"
+              title="video2"
+              src="https://www.youtube.com/embed/BYgMhIzRx-4"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </li>
+        </ul>
 
-
-
-
-    render() {
-        return (
-            <div className='videos'>
-                <ul id='videoList'>
-
-                    <li>
-                        🔎
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/4Q5b2r_sRX0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </li>
-
-                    <li>
-                        🔎<iframe width="560" height="315" src="https://www.youtube.com/embed/BYgMhIzRx-4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </li>
-                </ul>
-
-
-
-
-                <p id='disclamer'>
-                    <b>
-                        SOME VIDEOS ON THIS PAGE ARE NOT OWNED BY THIS PAGE. THIS PAGE DOES NOT GET ANY MONITARY GAINS FROM ANY VIDEOS HOSTED.
-
-                        </b>
-                </p>
-
-            </div>
-
-        )
-    }
-
-
-
-
-
-
+        <p id="disclamer">
+          <b>
+            SOME VIDEOS ON THIS PAGE ARE NOT OWNED BY THIS PAGE. THIS PAGE DOES NOT GET ANY MONITARY GAINS FROM ANY
+            VIDEOS HOSTED.
+          </b>
+        </p>
+      </div>
+    );
+  }
 }
-export default Videos
+export default Videos;
