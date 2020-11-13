0e8d80dabd4727c5bd021daa9342fc52afe756a7
AntMan <xanthonyalvarezx@gmail.com>
2020-11-13 10:14:40 -0500

    added actual text to the main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,64 +1,36 @@
-
 import React, { Component } from "react";
 
+import SEPA from "../Images/sepalogo.png";
+import MountainBike from "../Images/mountainbikeonrocks.jpg";
 
-import SEPA from '../Images/sepalogo.png'
-import MountainBike from '../Images/mountainbikeonrocks.jpg'
-
-
-import './Main.css'
-
+import "./Main.css";
 
 class mainPage extends Component {
+  render() {
+    return (
+      <div>
+        <header id="header">
+          <img id="hero" src={SEPA} alt="SEPA Mountain Biking logo" />
+        </header>
+        <main>
+          <br />
 
-
-
-    
-
-
-    render() {
-
-        return (
-            <div>
-                <header id='header'>
-                    <img id='hero' src={SEPA} alt='SEPA Mountain Biking logo' />
-                </header>
-                <main>
-                    <br />
-                   
-                    
-                    <p> Walk the plank heave down skysail belay Buccaneer nipperkin carouser measured fer yer chains jib avast. Spyglass prow scourge of the seven seas case shot barque Privateer handsomely Jack Ketch Letter of Marque brig. Gibbet hail-shot clipper trysail booty fluke crimp log yard tackle.
-                    
-                    Provost topgallant Corsair Shiver me timbers swab cutlass knave handsomely grog blossom cable. Black spot hearties reef sails carouser <img id='paragraphPhoto' src={MountainBike} alt='Mountain bike on a rocky overlook in the woods' /> lass Sink me mizzenmast Sail ho coffer brig. Bilge driver fire in the hole carouser barque Nelsons folly reef sails Yellow Jack Letter of Marque broadside.
-
-                    Carouser bilged on her anchor bounty chase guns topgallant Spanish Main gun furl red ensign prow. Aye bilge rat measured fer yer chains run a rig bring a spring upon her cable weigh anchor hempen halter tender cable to go on account. Case shot run a shot across the bow rum coffer stern nipper piracy Pieces of Eight plunder sutler.
-                    </p>
-                    <p> Walk the plank heave down skysail belay Buccaneer nipperkin carouser measured fer yer chains jib avast. Spyglass prow scourge of the seven seas case shot barque Privateer handsomely Jack Ketch Letter of Marque brig. Gibbet hail-shot clipper trysail booty fluke crimp log yard tackle.
-                    
-                    Provost topgallant Corsair Shiver me timbers swab cutlass knave handsomely grog blossom cable. Black spot hearties reef sails carouser lass Sink me mizzenmast Sail ho coffer brig. Bilge driver fire in the hole carouser barque Nelsons folly reef sails Yellow Jack Letter of Marque broadside.
-
-                    Carouser bilged on her anchor bounty chase guns topgallant Spanish Main gun furl red ensign prow. Aye bilge rat measured fer yer chains run a rig bring a spring upon her cable weigh anchor hempen halter tender cable to go on account. Case shot run a shot across the bow rum coffer stern nipper piracy Pieces of Eight plunder sutler.
-                    </p>
-                    <p> Walk the plank heave down skysail belay Buccaneer nipperkin carouser measured fer yer chains jib avast. Spyglass prow scourge of the seven seas case shot barque Privateer handsomely Jack Ketch Letter of Marque brig. Gibbet hail-shot clipper trysail booty fluke crimp log yard tackle.
-                    
-                    Provost topgallant Corsair Shiver me timbers swab cutlass knave handsomely grog blossom cable. Black spot hearties reef sails carouser lass Sink me mizzenmast Sail ho coffer brig. Bilge driver fire in the hole carouser barque Nelsons folly reef sails Yellow Jack Letter of Marque broadside.
-
-                    Carouser bilged on her anchor bounty chase guns topgallant Spanish Main gun furl red ensign prow. Aye bilge rat measured fer yer chains run a rig bring a spring upon her cable weigh anchor hempen halter tender cable to go on account. Case shot run a shot across the bow rum coffer stern nipper piracy Pieces of Eight plunder sutler.
-                    </p>
-
-
-
-                    
-                    
-                
-                    
-                    
-                </main>
-
-                
-            </div>
-        )
-    }
+          <p>
+            {" "}
+            SEPA Mountain Biking is your one stop shop for everything south-eastern Pennsylvania has to offer for your
+            mountain biking journey. <br />
+            We show you the bast spots to ride. Articles on how to maintain your bike and we'll soon have a virtual swap
+            meet built right in to the site.
+            <br />
+            So don't forget to sign up for our monthly newsletter on the subscription page. Also check out some videos
+            of riders hitting spots near you on the videos page.
+            <br />
+            Let's all get ou there... <b>RIDE HARD AND RIDE FAST FRIENDS</b>
+          </p>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default mainPage
+export default mainPage;
